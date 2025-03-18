97ac4be2cea99bb088d261253c58d2e91754202f
xinyking <xiny1643500702@outlook.com>
2025-03-19 02:55:11 +0800

    update name

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,8 +3,8 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = 'Yu Xin'
+export const headline = 'Student at SZU.'
 export const introduction =
   "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
 export const email = 'tong.hu@scls-sh.org'
