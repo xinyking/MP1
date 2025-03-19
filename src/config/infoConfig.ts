2a5e1ad8c7dfe3ee068caa00c0e2a706ba9e6058
xinyking <xiny1643500702@outlook.com>
2025-03-19 17:53:28 +0800

    update introduction

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -4,24 +4,23 @@ export * from './career'
 
 // personal info
 export const name = 'Yu Xin'
-export const headline = 'Student at SZU.'
+export const headline = 'Graduate student at Shenzhen University.'
 export const introduction =
-  "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
-export const email = 'tong.hu@scls-sh.org'
-export const githubUsername = 'derekhut'
+  "My name is Yu Xin. I am from Foshan, Guangdong, and I am a graduate student at Shenzhen University. My main research direction is network science."
+export const email = '2410095070@mails.szu.edu.cn'
+export const githubUsername = 'xinyking'
 
 // about page
 export const aboutMeHeadline = 'Who Are You and Why Should I Care?'
 export const aboutParagraphs = [
-  "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020.",
-  'Currently I am working on a new course called AI Camp, an innovative course where high school students create real-world products and startups while exploring the frontiers of AI.',
-  "I started this blog to share the insights I learn every day. Most blogs focus on education in Artificial Intelligence and general computer science, while others share the life lessons I've learned.",
+  "Coucou Les Amis! My name is Yu Xin. I'm from Guangdong and a graduate student at Shenzhen University. I’ve been learning Network Science since 2024.",
+  "I started this blog to share the insights I learn every day. Most blogs focus on education in Artificial Intelligence and general network science, while others share the life lessons I've learned.",
 ]
 
 // blog
 export const blogHeadLine = "What I've thinking about."
 export const blogIntro =
-  "I've written something about AI, programming and life."
+  "I've written something about Network science, programming and life."
 
 // social links
 export type SocialLinkType = {
@@ -35,12 +34,12 @@ export const socialLinks: Array<SocialLinkType> = [
   {
     name: 'Tiktok',
     icon: 'tiktok',
-    href: 'https://www.tiktok.com/@harvard?lang=en',
+    href: 'https://www.tiktok.com/',
   },
   {
     name: 'Bilibili',
     icon: 'bilibili',
-    href: 'https://space.bilibili.com/349721082',
+    href: 'https://space.bilibili.com/',
   },
 ]
 
